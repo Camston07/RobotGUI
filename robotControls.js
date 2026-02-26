02f287aaa2f0e6897016a11daee4d44f0ffac396
Camston07 <cameronsteeves02@gmail.com>
2026-02-26 16:09:28 -0400

    Created connect, disconnect, and publish functions

--- robotControls.js
+++ robotControls.js
@@ -1,4 +1,4 @@
-const brokerUrl = "ws://test.mosquitto.org:8080/mqtt";
+const brokerUrl = "wss://test.mosquitto.org:8081/mqtt";
 const topic = "m5stickcplus/robot/control";
 
 let client = null;
@@ -11,8 +11,8 @@ function main() {
 main();
 
 function setupConnectionButtons() {
-    document.getElementById("connectBtn").addEventListener("click", connectToMqttBroker());
-    document.getElementById("disconnectBtn").addEventListener("click", disconnectFromMqttBroker());
+    document.getElementById("connectBtn").addEventListener("click", connectToMqttBroker);
+    document.getElementById("disconnectBtn").addEventListener("click", disconnectFromMqttBroker);
 }
 
 function setupControlButtons() {
@@ -24,6 +24,40 @@ function setupControlButtons() {
     })
 }
 
-function sendCommand(cmd) {
+function connectToMqttBroker() {
+    if (client && client.connected) {
+        console.log("Already Connected");
+        alert("Already Connected");
+        return;
+    }
 
+    client = mqtt.connect(brokerUrl);
+
+    client.on("connect", () => {
+        console.log("Connected to MQTT broker");
+        alert("Connected");
+    });
+
+    client.on("close", () => {
+        console.log("Disconnected from MQTT broker");
+    });
+
+}
+
+function disconnectFromMqttBroker() {
+    if (client && client.connected) {
+        client.end();
+        alert("Disconnected from MQTT broker");
+    } else {
+        alert("Not Connected, can't disconnecte");
+    }
+}
+
+function sendCommand(cmd) {
+    if (client && client.connected) {
+        client.publish(topic, cmd);
+        console.log(`Send Command ${cmd}`);
+    } else {
+        console.log("Not connected, can;t send message");
+    }
 }
