f93b7a8567af9b5ffa59ae7a4bff01be4fbc19a5
Camston07 <cameronsteeves02@gmail.com>
2026-03-05 08:59:57 -0400

    Two connect options

--- robotControls.js
+++ robotControls.js
@@ -1,9 +1,4 @@
-// Private EMBED Broker
-// let topic = "EMBED/CamNev/m5stickcplus/robot/control";
-
-// Public Broker
-let topic = "m5stickcplus/robot/control";
-
+let topic = null;
 
 let client = null;
 
@@ -15,7 +10,8 @@ function main() {
 main();
 
 function setupConnectionButtons() {
-    document.getElementById("connectBtn").addEventListener("click", connectToMqttBroker);
+    document.getElementById("connectBtnPrivate").addEventListener("click", connectToPrivateMqttBroker);
+    document.getElementById("connectBtnPublic").addEventListener("click", connectToPublicMqttBroker);
     document.getElementById("disconnectBtn").addEventListener("click", disconnectFromMqttBroker);
 }
 
@@ -28,7 +24,7 @@ function setupControlButtons() {
     })
 }
 
-function connectToMqttBroker() {
+function connectToPrivateMqttBroker() {
     if (client && client.connected) {
         console.log("Already Connected");
         alert("Already Connected");
@@ -36,17 +32,42 @@ function connectToMqttBroker() {
     }
 
     // Private EMBED channel
-    // let brokerUrl = "ws://IoT.ScheduleCare.ca:8083";
-    // let userName = "Cam-n-Nev";
-    // let pwd = "6Z4hn/4X!PLj";
+    let brokerUrl = "ws://IoT.ScheduleCare.ca:8083";
+    let userName = "Cam-n-Nev";
+    let pwd = "6Z4hn/4X!PLj";
+    topic = "EMBED/CamNev/m5stickcplus/robot/control";
 
-    // client = mqtt.connect(brokerUrl, {
-    //     username: userName,
-    //     password: pwd,
-    // });
+    client = mqtt.connect(brokerUrl, {
+        username: userName,
+        password: pwd,
+    });
+
+    client.on("connect", () => {
+        console.log("Connected to MQTT broker");
+        alert("Connected");
+    });
+
+    client.on("error", (err) => {
+        console.error("Connection error: ", err);
+    });
+
+    client.on("close", () => {
+        console.log("Disconnected from MQTT broker");
+    });
+
+}
+
+function connectToPublicMqttBroker() {
+    if (client && client.connected) {
+        console.log("Already Connected");
+        alert("Already Connected");
+        return;
+    }
 
     // Public Mosquitto
     let brokerUrl = "wss://test.mosquitto.org:8081/mqtt";
+    topic = "m5stickcplus/robot/control";
+
     client = mqtt.connect(brokerUrl);
 
     client.on("connect", () => {
